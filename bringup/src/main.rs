5176a5354a4daf1f41af259c6627abf84ce8524d
Jonathan Rico <jonathan@rico.live>
2026-09-07 16:20:07 -0600

    add embassy OTP example

--- bringup/src/main.rs
+++ bringup/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+//! This example shows reading the OTP constants on the RP235x.
+
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_rp::otp;
+use embassy_time::Timer;
+use panic_probe as _;
+
+#[embassy_executor::main(executor = "embassy_rp::executor::Executor", entry = "cortex_m_rt::entry")]
+async fn main(_spawner: Spawner) {
+    let _ = embassy_rp::init(Default::default());
+
+    Timer::after_millis(10).await; // flash read delay. might be unnecessary.
+
+    let chip_id = unwrap!(otp::get_chipid());
+    info!("Unique id:{:X}", chip_id);
+
+    let private_rand = unwrap!(otp::get_private_random_number());
+    info!("Private Rand:{:X}", private_rand);
+
+    loop {
+        Timer::after_secs(1).await;
+    }
 }
