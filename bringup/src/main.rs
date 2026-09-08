923ba773a30a5f6593ce2d67b1bd3ec93ff1f490
Jonathan Rico <jonathan@rico.live>
2026-09-07 21:37:41 -0600

    make a build

--- bringup/src/main.rs
+++ bringup/src/main.rs
@@ -10,19 +10,24 @@ use embassy_rp::otp;
 use embassy_time::Timer;
 use panic_probe as _;
 
-#[embassy_executor::main(executor = "embassy_rp::executor::Executor", entry = "cortex_m_rt::entry")]
+#[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let _ = embassy_rp::init(Default::default());
 
     Timer::after_millis(10).await; // flash read delay. might be unnecessary.
 
-    let chip_id = unwrap!(otp::get_chipid());
-    info!("Unique id:{:X}", chip_id);
+    let chip_id = unwrap!(otp::get_chipid().map_err(|_| ()));
+    let private_rand = unwrap!(otp::get_private_random_number().map_err(|_| ()));
 
-    let private_rand = unwrap!(otp::get_private_random_number());
+    info!("Unique id:{:X}", chip_id);
     info!("Private Rand:{:X}", private_rand);
 
     loop {
         Timer::after_secs(1).await;
     }
+}
+
+#[defmt::panic_handler]
+fn panic() -> ! {
+    cortex_m::asm::udf()
 }
--- shell.nix
+++ shell.nix
@@ -1,11 +1,17 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {
+  overlays = [ (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz")) ];
+} }:
 
+let
+  rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+    extensions = [ "rust-src" "llvm-tools-preview" ];
+    targets = [ "thumbv8m.main-none-eabihf" ];
+  };
+in
 pkgs.mkShell {
   buildInputs = [
-    pkgs.cargo
-    pkgs.rustc
+    rustToolchain
     pkgs.rust-analyzer
-
-    pkgs.picotool # needs extra udev rules
+    pkgs.picotool
   ];
 }
