e6054832545e5bbc380bc05b11c3f5df3ddc60c8
Jonathan Rico <jonathan@rico.live>
2026-09-14 18:08:34 -0600

    add pinouts

---
+++ bringup/src/pinout-dev.rs
@@ -0,0 +1,25 @@
+// Perfboard pinout
+//
+// DISP
+// P13 - DISP_DC
+// P12 - DISP_RES
+// P14 - DISP_BLK
+//
+// DISP (SPI-1)
+// P09 - DISP_CS
+// P10 - DISP_SCK
+// P11 - DISP_MOSI (DI)
+//
+// SD (SPI-0)
+// P05 - SD_CS
+// P02 - SD_SCK
+// P03 - SD_MOSI
+// P04 - SD_MISO
+//
+// DAC
+// Pxx - DAC_RST
+// Pxx - DAC_CLKOUT (is this an input?)
+// P03 - LRCK / WS
+// P04 - SCLK / SCLK
+// P02 - SDIN / SD
+// P23 - MCLK
---
+++ bringup/src/pinout.rs
@@ -0,0 +1,45 @@
+// Motherboard pinout
+//
+// DISP
+// P09 - DISP_DC
+// P10 - DISP_RES
+// P11 - DISP_BLK
+//
+// SD/DISP (SPI-1)
+// P13 - DISP_CS
+// P17 - SD_CS
+// P14 - SD_SCK
+// P15 - SD_MOSI (DI)
+// P12 - SD_MISO (DO)
+//
+// EXT/DSP (SPI-PIO)
+// P00 - EXT_CS
+// P01 - DSP_EXT_INT
+// P05 - DSP_CS
+// P06 - DSP_SCK
+// P07 - DSP_MISO
+// P08 - DSP_MOSI
+//
+// USB HOST
+// P02 - USB_PIO_N
+// P03 - USB_PIO_P
+//
+// CAPSENSE - I2C-1
+// P04 - CAP_INT
+// P16 - DAC_INT
+// P18 - I2C_SDA
+// P19 - I2C_SCL
+//
+// DAC
+// P24 - DAC_RST
+// P25 - DAC_CLKOUT (is this an input?)
+// P20 - LRCK
+// P21 - SCLK
+// P22 - SDIN
+// P23 - MCLK
+//
+// BUTTONS
+// P26 - A0 - SW_SELECT
+// P27 - A1 - SW_DIRECTION
+// P28 - A2 - CHRG
+// P29 - A3 - VBAT_DIV
